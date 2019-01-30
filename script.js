3ece3b6262d1e9d321120f802d5e8744064ee405
Nikolay Filippov <nph@rambler.ru>
2019-01-30 21:11:48 +0300

    Add index.html, style.css and script.js for module 4 task.

---
+++ script.js
@@ -0,0 +1,32 @@
+// function set_content_in_divs(paragraphs) {
+// 	console.log("entered function")
+//     console.log(paragraphs)
+      
+//     $.each(paragraphs, function(i, d) {
+//     	p = $("#p-" + i)
+//     	p.html("<p>" + d + "</p>")
+//         console.log("i value: " + i)
+//         console.log("d value: " + d)
+      
+//      })
+// }
+
+// var par = $.getJSON("http://sf-pyw.mosyag.in/m04/api/forecasts");
+// console.log("par value: " + par);
+$("h1").click(function() {
+	console.log("clicked");
+	$.getJSON("http://sf-pyw.mosyag.in/m04/api/forecasts", function(data) {
+		console.log("read JSON");
+		$.each(data["prophecies"], function(i, d) {
+			console.log("i value: ", i);
+			console.log("d value: ", d);
+			$("#p-" + i).html("<p>" + d + "</p>")
+		})
+	})
+})
+
+$(".col").hover(function() {
+	$( this ).addClass( "hover" );
+}, function() {
+	$( this ).removeClass( "hover" );
+});
